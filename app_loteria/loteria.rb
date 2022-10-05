d464683a164ee308fe95685cc11eebe8e7426079
Bruno <brunosilvabrait@gmail.com>
2022-10-05 19:44:47 -0300

    Lógica para verificação de números repetidos dentro do array

--- app_loteria/loteria.rb
+++ app_loteria/loteria.rb
@@ -1,26 +1,51 @@
 class Lottery
   def generator
     @num_array = []
-
+    
     while @num_array.length < 15
       random_number = rand(1..25)
-      @num_array.push(random_number) unless @num_array.include?(random_number)
+      self.ordenate
+      if !self.verify_number(random_number)
+        @num_array.push(random_number)
+      end
     end
   end
+  
+  def ordenate
+    @num_array = @num_array.sort #Fazer pela lógica
+  end
 
-  def sort_numbers
-    @num_array = @num_array.sort
+  def verify_number(number)
+    start_array = 0
+    end_array = @num_array.length - 1
+
+    while start_array <= end_array
+      mid_array = (start_array + end_array) / 2
+
+      if @num_array[mid_array] == number
+        return true
+      end
+
+      if @num_array[mid_array] < number
+        start_array = mid_array + 1
+      end
+
+      if @num_array[mid_array] > number                     
+        end_array = mid_array - 1                   
+      end
+    end
+    return false
   end
 
   def print_values
-    @num_array.each do |nums|
-      puts nums
+    @num_array.each do |num|
+      puts num
     end
   end
 end
 
+
 generate = Lottery.new
 
 generate.generator
-generate.sort_numbers
 generate.print_values
