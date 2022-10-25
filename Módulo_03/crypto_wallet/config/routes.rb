e8953287001822ccabf84ed1bdd604d6bc0aff95
Bruno <brunosilvabrait@gmail.com>
2022-10-24 21:26:14 -0300

    Pequena exemplo de como apontar a rota

--- Módulo_03/crypto_wallet/config/routes.rb
+++ Módulo_03/crypto_wallet/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "welcome#index"
+
+  get '/inicio', to: 'welcome#index'# Leva para a pagina principal
+
+
   resources :coins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
