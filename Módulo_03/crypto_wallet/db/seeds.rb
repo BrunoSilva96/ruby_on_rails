ef6e5aabce56af60c221cf7ad6f74fdd907b5398
Bruno <brunosilvabrait@gmail.com>
2022-11-07 20:35:19 -0300

    Parte final da explicação do CRUD, Permissão e parâmetros, db:seed, e tasks

--- Módulo_03/crypto_wallet/db/seeds.rb
+++ Módulo_03/crypto_wallet/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Coin.create!(
+   description: "Bitcoin", 
+   acronym: "BTC",
+   url_image: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/%C3%8Dcone-Bitcoin-PNG.png"
+)
+
+Coin.create!(
+   description: "Etherium", 
+   acronym: "ETH",
+   url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgLXNMeG4OgFWrCWd2vi4gGx5vCb0RE0g8yg&usqp=CAU"
+)
+
+Coin.create!(
+   description: "Dash", 
+   acronym: "DASH",
+   url_image: "https://w7.pngwing.com/pngs/853/418/png-transparent-logo-dash-cryptocurrency-ethereum-steemit-ripple-coin-blue-text-trademark.png"
+)
