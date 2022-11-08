7a9645a35888ac5fba05c717e3a74c47178df163
Bruno <brunosilvabrait@gmail.com>
2022-11-08 19:30:33 -0300

    Refatorando e usando o yield, find_or_create_by, e inicio da modelagem de um CRUD

--- Módulo_03/crypto_wallet/db/seeds.rb
+++ Módulo_03/crypto_wallet/db/seeds.rb
@@ -6,21 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+spinner = TTY::Spinner.new("[:spinner]  Cadastrando moedas...")
+spinner.auto_spin
 
-Coin.create!(
-   description: "Bitcoin", 
-   acronym: "BTC",
-   url_image: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/%C3%8Dcone-Bitcoin-PNG.png"
-)
+coins = [
+   {  
+      description: "Bitcoin", 
+      acronym: "BTC",
+      url_image: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/%C3%8Dcone-Bitcoin-PNG.png"
+   },
+   {  
+      description: "Etherium", 
+      acronym: "ETH",
+      url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgLXNMeG4OgFWrCWd2vi4gGx5vCb0RE0g8yg&usqp=CAU"
+   },
+   {  
+      description: "Dash", 
+      acronym: "DASH",
+      url_image: "https://w7.pngwing.com/pngs/853/418/png-transparent-logo-dash-cryptocurrency-ethereum-steemit-ripple-coin-blue-text-trademark.png"
+   }
+]
 
-Coin.create!(
-   description: "Etherium", 
-   acronym: "ETH",
-   url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgLXNMeG4OgFWrCWd2vi4gGx5vCb0RE0g8yg&usqp=CAU"
-)
+coins.each do |coin|
+   Coin.find_or_create_by!(coin)
+end
 
-Coin.create!(
-   description: "Dash", 
-   acronym: "DASH",
-   url_image: "https://w7.pngwing.com/pngs/853/418/png-transparent-logo-dash-cryptocurrency-ethereum-steemit-ripple-coin-blue-text-trademark.png"
-)
+spinner.success("Cadastro concluído!")
--- Módulo_03/crypto_wallet/lib/tasks/dev.rake
+++ Módulo_03/crypto_wallet/lib/tasks/dev.rake
@@ -1,10 +1,22 @@
 namespace :dev do
   desc "Configura o ambiente de desenvolvimento"
   task setup: :environment do
-    puts %x(rails db:drop)
-    puts %x(rails db:create)
-    puts %x(rails db:migrate)
-    puts %x(rails db:seed)
+    if Rails.env.development?
+      show_spinner("Apagando o banco de dados...") { %x(rails db:drop) }
+      show_spinner("Criando o banco de dados...") { %x(rails db:create) }
+      show_spinner("Migrando o banco de dados...") { %x(rails db:migrate) }
+      show_spinner("Populando o banco de dados...") { %x(rails db:seed) }
+    else
+      puts "Você não está em ambiente de desenvolvimento!"
+    end
   end
 
+private
+
+  def show_spinner(msg_start, msg_end = "Concluído!")
+    spinner = TTY::Spinner.new("[:spinner]  #{msg_start}")
+    spinner.auto_spin
+    yield
+    spinner.success("#{msg_end}")
+  end
 end
