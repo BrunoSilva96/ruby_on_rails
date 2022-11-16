545261c401baf4aff3a6dbf7a9138cc66ff42db0
Bruno <brunosilvabrait@gmail.com>
2022-11-16 19:50:09 -0300

    i18n para Models, Removendo o helper e usando i18n, Cokkies e seassions, entendendo JS

--- Módulo_03/crypto_wallet/app/controllers/welcome_controller.rb
+++ Módulo_03/crypto_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires[Cookie]"
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires[Session]"
     @meu_nome = params[:nome]#Variavel params é uma variavel global padrão do Rails
     #No navegador, usamos o  "&" para que possamos adicionar outra variavel no parametro da URL
     @curso = params[:curso]
