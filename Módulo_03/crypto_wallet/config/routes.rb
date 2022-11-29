237b75ee4a297e540102891f70c4db2bf22e64e8
Bruno <brunosilvabrait@gmail.com>
2022-11-29 20:12:51 -0300

    Correção no layout

--- Módulo_03/crypto_wallet/config/routes.rb
+++ Módulo_03/crypto_wallet/config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
 
   get '/inicio', to: 'welcome#index'# Leva para a pagina principal
 
-
   resources :coins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
