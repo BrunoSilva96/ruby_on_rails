9260a96e901e26c5f9314e2e8698a57f681258aa
Bruno <brunosilvabrait@gmail.com>
2022-12-12 20:05:53 -0300

    Ajuste no sistema de layouts, ajuste no nome dos usuarios, pmetaprogramação com criação de metodo

--- Módulo_05/time_to_answer/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  #http_basic_authenticate_with name: "bruno", password: "123"
-
-  def index
-  end
-end
--- Módulo_05/time_to_answer/db/schema.rb
+++ Módulo_05/time_to_answer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_08_175107) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_224618) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,7 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_175107) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-  create_table "profiles", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -31,8 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_08_175107) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_profiles_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_profiles_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
