e8e490fe84e340e3728a401ad0ceabaac927fa16
Bruno <brunosilvabrait@gmail.com>
2022-12-26 19:46:37 -0300

    Cadastrando novos admins, Mostrando as notificações, Refatorando as views com partials

--- Módulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ Módulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -1,10 +1,23 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   before_action :verify_password, only: [:update]
-  before_action :set_admin, only: [:edit, :update]
+  before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
     @admins = Admin.all
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: "Administrador cadastrado com sucesso!"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -12,9 +25,17 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   def update
     if @admin.update(params_admin)
-      redirect_to admins_backoffice_admins_path, notice: "Adminisstrados atualizado com sucesso!"
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
-      render :edit, notice: "Erro na atualização do Administrador"
+      render :edit
+    end
+  end
+
+  def destroy
+    if @admin.destroy
+      redirect_to admins_backoffice_admins_path, notice: "Administrador excluído com sucesso!"
+    else
+      render :index
     end
   end
 
