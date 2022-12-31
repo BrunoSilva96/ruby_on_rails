8fe2448e36450225a3f0faed9a2b669d5d2b56b3
Bruno <brunosilvabrait@gmail.com>
2022-12-31 10:27:04 -0300

    Boa prática para o  MVC

--- Módulo_05/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
+++ Módulo_05/time_to_answer/app/controllers/admins_backoffice/questions_controller.rb
@@ -1,6 +1,7 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit]
 
   def index
     @questions = Question.all.order(:description).page(params[:page]).per(25)
@@ -8,6 +9,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def new
     @question = Question.new
+    
   end
 
   def create
@@ -46,5 +48,10 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
    
   def set_question
     @question = Question.find(params[:id])
-  end                    
+  end         
+  
+  def get_subjects
+    @subjects = Subject.all
+  end
+  
 end
