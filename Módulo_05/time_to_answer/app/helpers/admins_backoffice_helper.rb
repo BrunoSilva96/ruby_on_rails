41b22ae97a230c748e8f0090253d4b570c992343
Bruno <brunosilvabrait@gmail.com>
2023-01-02 18:54:51 -0300

    Incrementando i18n para os Models, i18n com parâmetros, 4 formas de criar um registro no ActiveRecord

--- Módulo_05/time_to_answer/app/helpers/admins_backoffice_helper.rb
+++ Módulo_05/time_to_answer/app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,9 @@
 module AdminsBackofficeHelper
+   def translate_attribute(object = nil, method = nil)
+      if object && method
+         object.model.human_attribute_name(method)
+      else
+         "Informe os parâmetros corretamente!"
+      end
+   end
 end
