bfa216e70f25c7a852c3d2d68cf954850481b2fb
Bruno <brunosilvabrait@gmail.com>
2023-01-07 20:41:19 -0300

    Usando métodos de classe no model

--- Módulo_05/time_to_answer/app/models/question.rb
+++ Módulo_05/time_to_answer/app/models/question.rb
@@ -5,4 +5,16 @@ class Question < ApplicationRecord
 
   #Kaminari
   #paginates_per 5
+
+  def self.search(page, term)
+    Question.includes(:answers)
+            .where("lower(description) LIKE ?", "%#{term.downcase}%")
+            .page(page).per(10)
+  end
+
+  def self.last_questions(page)
+    Question.all.includes(:answers).order('created_at desc').page(page).per(5)
+  end
+
 end
+ 
