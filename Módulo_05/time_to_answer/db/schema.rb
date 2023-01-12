8c01875d49120329412eb2ccb70acb5fe36ef6db
Bruno <brunosilvabrait@gmail.com>
2023-01-11 23:51:26 -0300

    Mostrando a quantidade de questões por asunto, Entendendo e usando o counter_cache

--- Módulo_05/time_to_answer/db/schema.rb
+++ Módulo_05/time_to_answer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_28_192535) do
+ActiveRecord::Schema.define(version: 2023_01_11_174646) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_12_28_192535) do
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "questions_count"
   end
 
   create_table "users", force: :cascade do |t|
