820445fd59e42342d073ac44b9a337178ded0947
Bruno <brunosilvabrait@gmail.com>
2023-01-20 15:21:35 -0300

    Validações de condicionais, Conhecendo o Action Mailer

--- Módulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
+++ Módulo_05/time_to_answer/app/controllers/admins_backoffice/admins_controller.rb
@@ -25,6 +25,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   def update
     if @admin.update(params_admin)
+      AdminMailer.update_email(current_admin, @admin).deliver_now
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
       render :edit
--- Módulo_05/time_to_answer/app/mailers/application_mailer.rb
+++ Módulo_05/time_to_answer/app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'noraply@example.com'
   layout 'mailer'
 end
