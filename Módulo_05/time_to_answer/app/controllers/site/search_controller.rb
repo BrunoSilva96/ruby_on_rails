e775c583ed78a7945e56dc55b906ec3c979ef9ec
Bruno <brunosilvabrait@gmail.com>
2023-01-27 19:15:43 -0300

    Full-text Search, Instalando o Elasticsearch, Full-test Search no Rails

--- Módulo_05/time_to_answer/app/controllers/site/search_controller.rb
+++ Módulo_05/time_to_answer/app/controllers/site/search_controller.rb
@@ -1,9 +1,29 @@
 class Site::SearchController < SiteController
    def questions
-      @questions = Question._search_(params[:page], params[:term])
+      @questions = Question.search(params[:term], page: params[:page], per_page: 5)
    end
 
    def subject
       @questions = Question._search_subject_(params[:page], params[:subject_id])
    end
 end
+
+
+
+# Instalando o Elasticsearch
+- sudo apt-get update
+- sudo apt-get install default-jdk
+- wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.0-amd64.deb
+- wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.0-amd64.deb.sha512
+- shasum -a 512 -c elasticsearch-7.3.0-amd64.deb.sha512 
+- sudo dpkg -i elasticsearch-7.3.0-amd64.deb
+- ps -p 1 para verificar qual cmd esta em execução 
+  - sudo systemctl start elastcsearch.service
+- E para testar se esta ativo rodar
+  - curl 'http://localhost:9200'
+  
+# Full-text Search no Rails
+- Adicionar a gem 'searchkick'
+- Adicionar no Model questions o searchkick
+- E Modificar em search_controller.rb
+- **Olhar arquivos para verificar as mudanças**
--- Módulo_05/time_to_answer/app/helpers/site_helper.rb
+++ Módulo_05/time_to_answer/app/helpers/site_helper.rb
@@ -4,9 +4,9 @@ module SiteHelper
     when 'index'
         "Últimas perguntas cadastradas..."
     when 'questions'
-        "Resultado para o termo \"#{params[:term]}\"..."
+        "Resultado para o termo \"#{sanitize params[:term]}\"..."
     when 'subject'
-        "Mostrando questões para o assunto \"#{params[:subject]}\"..."
+        "Mostrando questões para o assunto \"#{sanitize params[:subject]}\"..."
     end
   end
 end
